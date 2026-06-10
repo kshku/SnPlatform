258323141f03553603efe90da930ec0fd8cf4b81
kshku <1211shree@gmail.com>
2026-06-10 12:09:42 +0530

    refactor: rename SN_API → SN_PLATFORM_API

--- platform/include/snplatform/api.h
+++ platform/include/snplatform/api.h
@@ -1,9 +1,11 @@
+#pragma once
+
 #include <sncore/api_common.h>
 
 #if defined(SN_PLATFORM_STATIC)
-    #define SN_API
+    #define SN_PLATFORM_API
 #elif defined(SN_EXPORT)
-    #define SN_API SN_API_HELPER_EXPORT
+    #define SN_PLATFORM_API SN_API_HELPER_EXPORT
 #else
-    #define SN_API SN_API_HELPER_IMPORT
+    #define SN_PLATFORM_API SN_API_HELPER_IMPORT
 #endif
