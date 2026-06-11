1b966e6fdd2ff08b11959b1ca4c27ca00ee36fdd
kshku <1211shree@gmail.com>
2026-06-11 13:26:42 +0530

    style: remove indentation from top-level ARM64 ASM directives

--- platform/src/arch/arm64/platform.asm
+++ platform/src/arch/arm64/platform.asm
@@ -1,10 +1,10 @@
-    AREA |.text|, CODE, READONLY
+AREA |.text|, CODE, READONLY
 
-    EXPORT sn_cntvct_el0
+EXPORT sn_cntvct_el0
 
 sn_cntvct_el0 PROC
     mrs x0, cntvct_el0
     ret
     ENDP
 
-    END
+END
